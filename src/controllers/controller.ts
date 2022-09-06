f4aaa2fd37b9712316e2902e1eaa08c0dd79aa29
arthur-sudbrack-ibarra <arthur.sudbrack.ibarra@hp.com>
2022-09-06 11:30:16 -0300

    More Skeletons.

--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -1,9 +1,44 @@
 import { Request, Response } from "express";
 
-export function login(_req: Request, _res: Response): void {}
-export function getAllUsers(_req: Request, _res: Response): void {}
-export function getUserById(_req: Request, _res: Response): void {}
-export function createUser(_req: Request, _res: Response): void {}
-export function updateUser(_req: Request, _res: Response): void {}
-export function updateUserPassword(_req: Request, _res: Response): void {}
-export function deleteUser(_req: Request, _res: Response): void {}
+export function login(_req: Request, res: Response): void {
+  // Call the service layer here.
+  res.json({
+    status: "OK! This is the login route.",
+  });
+}
+export function getAllUsers(_req: Request, res: Response): void {
+  // Call the service layer here.
+  res.json({
+    status: "OK! This is the get all users route.",
+  });
+}
+export function getUserById(req: Request, res: Response): void {
+  // Call the service layer here.
+  res.json({
+    status: `OK! This is the get user by id route. ID: ${req.params.id}`,
+  });
+}
+export function createUser(_req: Request, res: Response): void {
+  // Call the service layer here.
+  res.json({
+    status: "OK! This is the create user route.",
+  });
+}
+export function updateUser(req: Request, res: Response): void {
+  // Call the service layer here.
+  res.json({
+    status: `OK! This is the update user route. ID: ${req.params.id}`,
+  });
+}
+export function updateUserPassword(req: Request, res: Response): void {
+  // Call the service layer here.
+  res.json({
+    status: `OK! This is the update user password route. ID: ${req.params.id}`,
+  });
+}
+export function deleteUser(req: Request, res: Response): void {
+  // Call the service layer here.
+  res.json({
+    status: `OK! This is the delete user route. ID: ${req.params.id}`,
+  });
+}
