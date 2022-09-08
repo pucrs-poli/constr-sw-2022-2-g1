0a0f50c022c99d40a2e262a893d06fdbf10e36bc
arthur-sudbrack-ibarra <arthur.sudbrack.ibarra@hp.com>
2022-09-08 20:34:46 -0300

    Update client secret.

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const REALM_NAME = "constr-sw-2022";
 export const CLIENT_ID = "grupo1";
-export const CLIENT_SECRET = "pCHwUi4ZHW9RnQzMN1yMGjpPxBtvufkG";
+export const CLIENT_SECRET = "boaeAxHFXBf6Rf40Fqgz6xtQuTyQUCBU";
