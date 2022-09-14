d7fb1235bd905c248a7acd685448997289923849
Miguel Castro <69170175+Miguel-de-Castro@users.noreply.github.com>
2022-09-14 01:28:19 -0300

    Add realm import file

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@
 */
 export const KEYCLOAK_PORT = 8080;
 
-export const REALM_NAME = "constr-sw-2022";
+export const REALM_NAME = "constr-sw-2022-2";
 
 export const CLIENT_ID = "grupo1";
 export const CLIENT_SECRET = "boaeAxHFXBf6Rf40Fqgz6xtQuTyQUCBU";
