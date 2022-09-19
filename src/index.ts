30d18b92d2ad269f18ecb2b6d75c863089f57d4b
MagicManatee <arthur.ibarra@edu.pucrs.br>
2022-09-19 18:36:54 -0300

    Remove swagger auto open.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import swaggerUi from "swagger-ui-express";
-import { execSync } from "child_process";
 import swaggerDocument from "./docs/swagger.json";
 import { API_PORT } from "./config";
 import Router from "./routes/router";
@@ -9,22 +8,14 @@ import Router from "./routes/router";
 const app = express();
 app.use(express.json());
 app.use(cors());
+
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+const SWAGGER_ENDPOINT = `http://localhost:${API_PORT}/api-docs`;
 
 const router = new Router(app);
 router.setupRoutes();
 
-const OPEN_URL_COMMAND =
-  process.platform == "darwin"
-    ? "open"
-    : process.platform == "win32"
-    ? "start"
-    : "xdg-open";
-
-const SWAGGER_ENDPOINT = `http://localhost:${API_PORT}/api-docs`;
-
 app.listen(API_PORT, () => {
   console.log(`Backend running on port ${API_PORT}.\n`);
   console.log(`Swagger docs available at ${SWAGGER_ENDPOINT}.\n`);
-  execSync(`${OPEN_URL_COMMAND} ${SWAGGER_ENDPOINT}`);
 });
