6329fac4cae3bd8f1f22cfa51a4a3ae7840a36b1
MagicManatee <arthur.ibarra@edu.pucrs.br>
2022-09-19 14:31:36 -0300

    Open swagger on npm start.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import swaggerUi from "swagger-ui-express";
+import { execSync } from "child_process";
 import swaggerDocument from "./docs/swagger.json";
 import { API_PORT } from "./config";
 import Router from "./routes/router";
@@ -13,9 +14,17 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 const router = new Router(app);
 router.setupRoutes();
 
+const OPEN_URL_COMMAND =
+  process.platform == "darwin"
+    ? "open"
+    : process.platform == "win32"
+    ? "start"
+    : "xdg-open";
+
+const SWAGGER_ENDPOINT = `http://localhost:${API_PORT}/api-docs`;
+
 app.listen(API_PORT, () => {
   console.log(`Backend running on port ${API_PORT}.\n`);
-  console.log(
-    `Swagger docs available at http://localhost:${API_PORT}/api-docs.\n`
-  );
+  console.log(`Swagger docs available at ${SWAGGER_ENDPOINT}.\n`);
+  execSync(`${OPEN_URL_COMMAND} ${SWAGGER_ENDPOINT}`);
 });
