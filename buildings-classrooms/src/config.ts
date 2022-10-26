4b1115f4ce6102420a6af36994bad121de3abdfd
arthur-sudbrack-ibarra <arthur.sudbrack.ibarra@hp.com>
2022-10-26 20:29:51 -0300

    Initial configuration.

---
+++ buildings-classrooms/src/config.ts
@@ -0,0 +1,10 @@
+/*
+    Keycloak API configuration.
+*/
+export const KEYCLOAK_API_HOST = process.env.KEYCLOAK_API_HOST || "localhost";
+export const KEYCLOAK_API_PORT = 3000;
+
+/*
+    API configuration.
+*/
+export const API_PORT = 8081;
